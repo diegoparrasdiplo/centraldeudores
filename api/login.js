14bf83d9eca13adffd8e621b7cf164b92db07f98
diegoparrasdiplo <diegoparrasdiplo@gmail.com>
2026-04-07 09:03:09 -0300

    Add files via upload

--- api/login.js
+++ api/login.js
@@ -1,5 +1,4 @@
-export default function handler(req, res) {
-  // CORS
+module.exports = function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -15,4 +14,4 @@ export default function handler(req, res) {
   } else {
     return res.status(401).json({ ok: false });
   }
-}
+};
